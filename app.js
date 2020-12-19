14066d6607e17fad94f4c64dc03b746fe1707113
naratipcover <naratipcover@gmail.com>
2020-12-19 16:11:30 +0700

    edit text

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Sup!'
+            text: '5555565'
         },
         {
             type: 'text',
